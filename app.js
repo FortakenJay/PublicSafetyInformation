3d76b08c1160c68f49993c51c55bf9bb8eafcfa8
DavidHolleman <115103613+DavidHolleman@users.noreply.github.com>
2023-11-29 00:00:44 -0600

    cleanup code, fix paths, and add placeholders

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const app = express();
 //register for view engine
 app.set('view engine', 'ejs');
 
-
 //middleware and static files aka css
 app.use(morgan('dev'));
 app.use(express.static('css'));
@@ -16,13 +15,10 @@ app.use(express.static('images'));
 //listen for request
 app.listen(3000);
 
-
 // response website
 app.get('/', (req,res) => {
     res.render('main');
 });
-
-
 
 //404 page MUST BE BELOW EVERYTHING
 app.use((req,res) => {
